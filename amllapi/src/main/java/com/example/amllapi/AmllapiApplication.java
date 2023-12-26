1a964f20d997d580e839ba9f01eaa7622d0e5d20
youss95 <usekim7555@gmail.com>
2023-12-26 20:08:50 +0900

    commit

---
+++ amllapi/src/main/java/com/example/amllapi/AmllapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amllapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmllapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmllapiApplication.class, args);
+    }
+
+}
